5ebfe7bf3de35ffb88b75e107215d22cd7979326
Lup1n5 <aidan.korten@outlook.com>
2026-07-01 14:22:55 -0400

    soem work

---
+++ pinouts.md
@@ -0,0 +1,5 @@
+A0-5 = nothing
+0-2 = ch1-3, 4,7,8 = 4, 5, 6  FSiA6B
+3,5 motor A PWM
+6, 9 motor B PWM
+10- 13 = stepper in order 
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#include <battle-bots-lib.h>
+const int DEADBAND = 1;
+Motor input1(1, 20, 130, 255); //ele
+Motor input2(2, 20, 130, 255); //ail
+Motor input3(3, 20, 130, 255); 
+Motor input4(4, 20, 130, 255);
+Motor input5(5, 20, 130, 255);
+Motor input6(6, 20, 130, 255);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  //Serial.print(motor1.getJoystickValue() + "    ");
+  //delay(0.03);
+  int speed = input1.getOutput();
+  //Serial.println(speed);
+  if (speed == 0) {
+  analogWrite(8,0);
+  analogWrite(9,0); 
+  } 
+//  else {
+//    analogWrite(8,speed);
+//    analogWrite(9,0);
+//    }
+   
+    else if (speed >0) {
+    analogWrite(8,speed);
+    analogWrite(9,0);
+  } else {
+    analogWrite(8,0);
+    analogWrite(9,abs(speed));
+  }
 }
